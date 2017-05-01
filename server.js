f024501801ed679f8b2e693a77761e7e69853977
Kyle <harr1193@msu.edu>
2017-05-01 15:08:08 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 8080
+
+const pages = [
+{url: '/lions', name: 'Detroit Lions', img: '/img/lions.jpg', link: 'http://www.detroitlions.com/'},
+{url: '/tigers', name: 'Detroit Tigers', img: '/img/tigers.jpg', link: 'https://www.mlb.com/tigers/'},
+{url: '/pistons', name: 'Detroit Pistons', img: '/img/pistons.jpg', link: 'http://www.nba.com/pistons//'}
+]
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
+
+app.get('/', function (request, response) {
+  response.render('pages/index', {
+    pages: pages
+  })
+})
+
+pages.forEach(function (page) {
+  app.get(page.url, function (request, response) {
+    response.render('pages/teams.ejs', {
+      page: page
+    })
+  })
+})
+
+app.listen(port)
